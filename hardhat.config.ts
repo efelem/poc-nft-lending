ca49007a1a8fe10731c586dd3be323d5cf5a2fc0
Florin Dzeladini <srill.fb99@gmail.com>
2024-03-29 13:19:25 +0100

    config: hardhat localhost node added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,11 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    localhost: {
+      url: "http://localhost:8545", // URL of your local testnet node
+    },
+  },
 };
 
 export default config;
